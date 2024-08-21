387c7fae1c54db2cbf2c539e274ebff4fc91606e
Jakub Zalewski <jakub.zalewski93@gmail.com>
2024-08-21 13:12:55 +0200

    Delete static directory

--- static/two_sum.py
+++
@@ -1,7 +0,0 @@
-def two_sum(nums, target):
-    # your code here
-
-# Test cases
-nums = [2, 7, 11, 15]
-target = 9
-print(two_sum(nums, target)) # Output: [0, 1]
--- static/valid_parentheses.py
+++
@@ -1,9 +0,0 @@
-def is_valid_parentheses(s: str) -> bool:
-    # your code here
-
-# Example usage:
-print(is_valid_parentheses("()"))       # True
-print(is_valid_parentheses("()[]{}"))   # True
-print(is_valid_parentheses("(]"))       # False
-print(is_valid_parentheses("([)]"))     # False
-print(is_valid_parentheses("{[]}"))     # True
